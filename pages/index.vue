b27d46d9fa22931f62cecf5943f7abcb8cb08b79
krrrr38 <k.kaizu38@gmail.com>
2023-11-01 05:59:31 +0900

    resolve fetch blogs based on locale closes #1

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,7 @@
 <template>
   <!-- LazyをPrefixにつけることで遅延読み込みになる -->
   <LazyTopMainVisual />
-  <!-- https://github.com/scalamatsuri/2024.scalamatsuri.org/issues/1: Newsコンポーネントにてブログの内容が取得できないため修正する -->
-  <!-- <TopNews /> -->
+  <TopNews />
   <TopEvents />
   <TopCodeOfConduct />
   <TopSponsors />
