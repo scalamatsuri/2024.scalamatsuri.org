4259175a293266e45d8b0d92e5d6008d7e37f64a
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2023-11-03 19:34:00 +0900

    テキストを2023年のCoCからそのまま移植

--- pages/code-of-conduct/index.vue
+++ pages/code-of-conduct/index.vue
@@ -1,3 +1,119 @@
+<i18n lang="yaml">
+## language=yaml
+en:
+  title: "Code of Conduct"
+  coc_manner_video_title: "CoC Manner Video"
+  code_of_conduct_text_01: "We showed a video for Code of conduct and referee system in ScalaMatsuri 2016 opening."
+  code_of_conduct_title: "Code of conduct"
+  preface_title: "Preface"
+  preface_text_01: |
+    ScalaMatsuri is dedicated to providing an open conference for all technologists from various regions and communities. We want to make it a fun experience for everyone, inclusive of gender, race, and many different backgrounds whom they may not meet in their daily life.
+    <br><br>
+    The following Code of conduct is a guideline to prevent all of us from making inconsiderate behavior, intentional or otherwise. The ScalaMaturi comittee requires all participants, including speakers and sponsors to comply with the Code of conduct as part of our active step to prevent inappropriate behaviors.
+  body_title: "Body"
+  body_text_01: |
+    All communication is expected to be appropriate for a technology conference with professional audience including people of many different backgrounds. ScalaMatsuri does not tolerate harassment of conference participants in any form.
+    <br><br>
+    We expect all participants, including guests, exhibitors, sponsors, booth staff, and the organizers, to follow these rules at all conference venues, conference-related social events and communications in SNS. Participants asked to stop any harassing behavior (regardless of the intention) are expected to comply immediately.
+    <br><br>
+    Harassment includes, but is not limited to:
+  body_text_02:
+    - Asking other participants out (comments about appearance, romatic or sextual interest) or inappropriate physical contact.
+    - Offensive verbal comments or discriminatory behaviors related to gender, gender identity and expression, sexual orientation, disability, physical appearance, body size, age, race, nationality, ethnicity, or religion.
+    - Sexual images in public spaces.
+    - Deliberate intimidation; stalking, following, harassing photography or recording.
+    - Sustained disruption of talks or other events.
+    - Use of sexualized images, activities, or other material during the session.
+    - Use of sexualized clothing/uniforms/costumes by the booth staff or volunteers, or otherwise creating sexualized environment in the booth or the displayed material.
+  body_text_03: |
+    If a participant engages in harassing behavior, the conference organizers may take actions like giving a citation or a warning. If the harassing behavior does not stop regardless of the warning and recognized as unruly incident, the conference organizers may expulse from the conference with no refund. All participants, including guests, exhibitors, sponsors, booth staff, must comply to the order by the conference organizers immediately without condition.
+    <br><br>
+    The code of conduct is subject to change without notice.
+    <br><br>
+    Similar code of conduct have been adopted by PNW Scala, NE Scala, and Scala Days to provide a harassment-free conference. We also support the cause.
+  body_text_04: "The following descibes the operational process by the conference organizers. It is subject to change without notice."
+  process_title: Process
+  process_text_01:
+    - The conference organizers are to prepare an incident reporting form both in offline and online. Upon a report, adequate actions will be taken like giving a citation or a warning to prevent from repeating the incident.
+    - All sponsors are asked to comply with the Code of conduct when they apply. Their slides and commercial videos are checked beforehand and modified if necessary.
+    - All speakers are asked to comply with the Code of conduct when they apply to CFP. Their slides are checked beforehand and modified if necessary.
+    - In Open mic conference, the conference organizers will ask speakers to be checked to comply with Code of conduct before their talks. Due the nature of Open mic conference, it will be difficult to guarantee prior check of all contents, so this will require cooperation from all participants.
+    - At the conference venues, participants are free to use bathrooms based on their own self-declared gender identity. A non-binary person is allowed to use any bathrooms.
+  contacts_during_the_conference_title: Contacts during the conference
+  contacts_during_the_conference_text_01: |
+    If you are being harassed, notice that someone else is being harassed, please report the following form or contact a nearby conference staff immediately.
+    <br><br>
+    <a href="https://forms.gle/zu8x1jNBepbHZKXM8" target="_blank" rel="noopener">Harassment incidents report form.</a>
+  license_and_attribution_title: License and attribution
+  license_and_attribution_text_01: |
+    The text part of this policy is licensed under the <a href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank" rel="noopener">Creative Commons Zero license</a>. Like public domain, no credit and no open licencing of your version is required.
+    <br><br>
+    If you would like to optionally attribute it, you could use the below text and link to <a href="http://scalamatsuri.org/" target="_blank" rel="noopener">http://scalamatsuri.org/</a>:
+  license_and_attribution_text_02: |
+    This code of conduct is based on the policy from ScalaMatsuri, which in turn is based on the example policy from the Geek Feminism wiki, and inspired by PNW Scala, NE Scala, and Scala Days.
+  license_and_attribution_text_03: |
+    Japan Scala Association, Inc reserves all rights for the CoC Manner Video. If you agree to our code of conduct as shown as above, we permit the use of the video in your conference or meetup.
+    <br><br>
+    We would also appreciate it if you let us know via e-mail to <a href="mailto:info@scalamatsuri.org">info@scalamatsuri.org</a> or tweet with hashtag <a href="https://twitter.com/hashtag/ScalaMatsuri" target="_blank" rel="noopener">#ScalaMatsuri</a>.
+ja:
+  title: 行動規範
+  coc_manner_video_title: 行動規範マナー動画
+  code_of_conduct_text_01: ScalaMatsuri2016当日には、行動規範とレフェリー制について解説した動画を上映しました。
+  code_of_conduct_title: 行動規範
+  preface_title: はじめに
+  preface_text_01: |
+    ScalaMatsuri は、様々な地域やコミュニティから集う技術者に対して開かれたカンファレンスを目指しています。 そのためには、性別や人種など多様な背景を持つ、普段の生活では会わない人々同士でも、互いに敬意を払って楽しい時間を過ごせることが重要だと考えています。
+    <br/><br/>
+    以下の行動規範は、意図せずそういった配慮の行き届かない発言や行為をしてしまうことを防ぐためのガイドラインです。 ScalaMatsuri の主催者は、発表者や参加者、スポンサーの皆様に行動規範を守っていただくことをお願いしており、その場にそぐわない発言や行為を未然に防ぐための手助けをしています。
+  body_title: 本文
+  body_text_01: |
+    多様な背景を持つ人々が参加する技術カンファレンスにおいて、そこで交わされるコミュニケーションは技術的な発表と交流の場に相応しいものであって欲しいと願っています。 ScalaMatsuri は、カンファレンスの参加者に対するいかなるハラスメント行為も歓迎しません。
+    <br/><br/>
+    ScalaMatsuri は、会場内および関連するソーシャルイベント、SNS上でのコミュニケーションの全てにおいて、参加者、発表者、スポンサー、ブース出展者など、全ての関係者の皆様に対して本行動規範の遵守を求めます。 カンファレンスの参加者および関係者は、自身のハラスメント行為（意識的、無意識的を問わず）について他者から指摘を受けた場合は、直ちにその行動を中止することを期待されています。
+    <br/><br/>
+    ハラスメント行為の一例には以下のようなものがあります:
+  body_text_02:
+    - 他の参加者に対するナンパ行為 (容姿に関する発言、恋愛・性的興味を目的とした発言) や不適切な身体的接触を行うこと
+    - ジェンダー、性自認、ジェンダーの表出、性的指向、障がい、容貎、身体の大きさ、年齢、人種、国籍、民族、宗教について、当人が不快に感じる発言や差別を助長する言動を行うこと
+    - 公共の場に性的な画像を掲示したり、見せびらかしたりすること
+    - 他の参加者に対して、故意の威嚇やストーキング、つきまとい、本人が嫌がらせと感じるような写真撮影や録音録画を行うこと
+    - カンファレンスの発表や、その他のイベントを継続的に妨害し続けること
+    - カンファレンスの発表において、発表資料に性的な画像や素材などを使用したり、性的な演出を行うこと
+    - 会場内のブースや掲示物において、出展スタッフやボランティアが性的な服装、制服、コスチュームを着用したり、その他の方法で性的な雰囲気を演出すること
+  body_text_03: |
+    ScalaMatsuri の主催者は、本行動規範の趣旨に反してハラスメント行為を行う参加者に対して注意や警告を行います。 警告に従わずハラスメント行為を繰り返す場合や悪質な場合など、明らかな迷惑行為であると判断できる場合には、発表の中止やカンファレンス会場からの退場の指示を主催者の裁量で行うことがあります。
+    <br/><br/>
+    当カンファレンスの参加者、発表者、スポンサー、ブースの出展者は、主催者の指示に即時かつ無条件に従ってもらえることを期待します。 また、主催者の裁量によって会場から退場を指示された場合、該当者に対する参加料等の金銭の払い戻しは行わないものとします。
+    <br/><br/>
+    同種の行動規範は、ハラスメントの無いカンファレンスを提供することを目指して、例年 PNW Scala、NE Scala、Scala Days などでも採用されており、ScalaMatsuri もその精神に賛同します。
+    <br/><br/>
+    この行動規範はより適切な運用を行うために随時更新される可能性があります。
+  process_title: 運用方法
+  body_text_04: ScalaMatsuri では、行動規範について以下の通り運用します。また必要に応じて、新たなプロセスを設ける可能性が有ります。
+  process_text_01:
+    - インシデントの報告窓口をオンライン及びオフラインで設けます。インシデントが報告された場合、主催者は同様のインシデントが繰り返し発生しないように努め、必要に応じて注意や警告を行います。
+    - ScalaMatsuriの全てのスポンサーに対し、スポンサー申込時に行動規範準拠の同意を確認しています。また、スライドやCMといった上映コンテンツの事前チェックによる行動規範準拠の確認、そして必要な場合は修正を依頼しています。
+    - カンファレンスにおける全ての発表者に対し、CFPへの応募時に行動規範準拠の同意を確認しています。また、スライドの事前チェックによる行動規範準拠の確認、そして必要な場合は修正を依頼しています。
+    - 飛び入りカンファレンスでは、全ての発表者に対しスライドの事前チェックを受けるように呼びかけます。飛び入りカンファレンスの性質上、漏れのない事前チェックを保証することは難しいので、皆さんのご協力が特に不可欠です。よろしくお願いします。
+    - 会場のお手洗いは、ご本人が自己宣言したジェンダーアイデンティティに基づいて利用していただいています。非バイナリジェンダーの方は、どのお手洗いでも利用可能としています。
+  contacts_during_the_conference_title: 会期中の報告窓口
+  contacts_during_the_conference_text_01: |
+    自分や他の人がハラスメントを受けている場合には以下のフォームにてご連絡ください。
+    <br/><br/>
+    <a href="https://forms.gle/zu8x1jNBepbHZKXM8" target="_blank">ハラスメント インシデント報告フォーム</a>
+  license_and_attribution_title: ライセンスと帰属に関して
+  license_and_attribution_text_01: |
+    本規範のテキスト部分は <a href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank">Creative Commons Zero license</a> のもとに公開します。パブリック・ドメイン同様に、派生して作られた規範に一切のクレジット表示やオープンライセンスなどを要求しません。
+    <br><br>
+    もし、任意に帰属表示をしていただけるならば、<a href="http://scalamatsuri.org/" target="_blank">http://scalamatsuri.org/</a> へリンクして以下のようなテキストをお使いください:
+  license_and_attribution_text_02: |
+    本規範は ScalaMatsuri の規範に基いています。ScalaMatsuri の規範は Geek Feminism wiki の規範例から派生しており、PNW Scala、NE Scala、および Scala Days の影響を受けています。
+  license_and_attribution_text_03: |
+    行動規範マナー動画に関しましては、一般社団法人 Japan Scala Association が著作権を保持します。ただし、この行動規範に同意してくださる限りにおいて、このビデオをカンファレンスや勉強会­、Meetupなどでご自由に使用して頂いて構いません。
+    <br><br>
+    もしご利用された場合には、<a href="mailto:info@scalamatsuri.org">info@scalamatsuri.org</a> か、ハッシュタグ <a href="https://twitter.com/hashtag/ScalaMatsuri" target="_blank">#ScalaMatsuri</a> をつけたツイートでご一報くださるととても嬉しいです。
+</i18n>
+
 <template>
   {{ $route.fullPath }}
 </template>
