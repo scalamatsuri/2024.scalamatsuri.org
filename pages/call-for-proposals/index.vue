73ca36e60957f31c3b6a38dccb1e6471e14a8eb4
omiend <omiend@gmail.com>
2023-12-23 11:22:17 +0900

    日本語の文言修正＆重複した<li>を削除

--- pages/call-for-proposals/index.vue
+++ pages/call-for-proposals/index.vue
@@ -6,8 +6,7 @@ en:
     We are excited to announce that ScalaMatsuri will be held on June 7th (Friday) to June 9th (Sunday). Please submit your talk for the general sessions.<br/><br/>
     The CFP will close at February 24, 2024 (Saturday) at 23:59 <a href="https://time.is/ja/Anywhere%20on%20Earth">Anywhere on Earth</a> (20:59 JST on February 25).
     <br/><br/>
-    Matsuri, meaning festival, is about time and space out of the everyday.<br>
-    It’s about mustering up the courage to do something cool (even though we are normally shy).<br>
+    Matsuri, meaning festival, is about time and space out of the everyday.It’s about mustering up the courage to do something cool (even though we are normally shy).<br>
     We want to make it a fun experience, regardless of gender, race, and many different backgrounds.<br>
     All participants, including speakers and sponsors, are asked to be respectful to each other and follow the <a href="%{coc_link}" target="_blank" rel="noopener"><span>code of conduct</span></a>.
     <br><br>
@@ -36,7 +35,6 @@ en:
       <li class="section_note_item">The talk must be in either English or Japanese. All slides must be in English. Note that we will prepare Japanese subtitles.</li>
       <li class="section_note_item">The abstract must be 300 characters or fewer. We might ask for a longer overview once your talk is accepted.</li>
       <li class="section_note_item">You are allowed up to 3 submissions.</li>
-      <li class="section_note_item">Each person can apply for up to three sessions.</li>
       <li class="section_note_item">Please note that if you exceed any of the limits, the staff may edit your submissions at our discretion.</li>
     </ul>
     Note again that ScalaMatsuri organizers will need to prepare the subtitles, so you are asked to submit the slides weeks in advance.
@@ -65,14 +63,13 @@ en:
     Suppose you’ve been approved for the grant and the travel expense was $1500 from Europe, the maximum amount the ScalaMatsuri will reimburse will be $1000. If you started from somewhere in Asia and it cost $700, the reimbursement will be $700.<br/><br/>
     Your expenses will NOT be accepted in the event that you do not attend the conference. At the sole discretion of ScalaMatsuri, some expenses may be reimbursed upon a natural disaster, death or illness of the speaker or a family member. We suggest you purchase travel insurance if these terms are not sufficient.
 ja:
-  title: 'ScalaMatsuriで話を聞かせてください！'
+  title: ScalaMatsuriで話を聞かせてください！
   section1_body: |
-    2024年6月7日(金) - 6月9日(日)に ScalaMatsuri 2024 を開催します。<br/>
-    そこで、ScalaMatsuri 2024の一般セッションの応募を開始します。<br/>
+    2024年6月7日(金) - 6月9日(日)に ScalaMatsuri 2024 を開催します。そこで、ScalaMatsuri 2024の一般セッションの応募を開始します。<br/>
     締切は 2024年2月24日(土) 23:59 <a href="https://time.is/ja/Anywhere%20on%20Earth">Anywhere on Earth</a>(2月25日 20:59 JST)を予定しています。<br/><br/>
-    「Matsuri」はハレの日、つまり非日常の時と空間です。
-    (普段はシャイでも) ちょっと勇気を出してちょっとカッコいいことをやってみたい。そういった性別や人種など多様な背景を持つ人々が互いに敬意を払って楽しい時間を過ごせるよう、当カンファレンスでは発表者や参加者、スポンサーの皆さんに<a href="%{coc_link}" target="_blank" rel="noopener"><span>行動規範</span></a>を守っていただくようにお願いしています。<br/>
-    アジア最大規模の国際的なイベントとして、日本国内だけではなく、海外からの参加者にも楽しめるイベントにしたいと考えています。<br/><br/>
+    「Matsuri」はハレの日、つまり非日常の時と空間です。(普段はシャイでも) ちょっと勇気を出してトークしてみませんか。あなたのハレ舞台となるでしょう。<br/><br/>
+    ScalaMatsuri 2024 はアジア最大規模の国際的なイベントとして、日本国内だけではなく、海外からの参加者にも楽しめるイベントにしたいと考えています。<br/>
+    性別や人種など多様な背景を持つ人々が互いに敬意を払って楽しい時間を過ごせるよう、当カンファレンスでは発表者や参加者、スポンサーの皆さんに<a href="%{coc_link}" target="_blank" rel="noopener"><span>行動規範</span></a>を守っていただくようにお願いしています。<br/>
     セッション募集に際してブログも公開しております。こちらも併せてご確認ください。<br/>
     <a href="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX">ScalaMatsuri 2024開催決定！CFPも募集開始！</a><br/><br/>
     今年度もScalaMatsuri2023と同様に、オープンマイクセッションを2日間開催します。<br/>
@@ -92,7 +89,6 @@ ja:
     <ul class="section_note">
       <li class="section_note_item">内容は、Scala ビギナー もしくは Scala コミュニティが興味があると思うトピックであれば自由です。</li>
       <li class="section_note_item">講演の時間は20分枠となります。40分枠もございますが、今回は20分枠が中心のイベントになります。</li>
-      <li class="section_note_item">提出可能なプロポーザルは 1 人最大 3 つまでです。</li>
       <li class="section_note_item">講演は通常の一人が聴衆に対して喋る講義形式でお願いします。</li>
       <li class="section_note_item">話す言語は、英語もしくは日本語です。スライド本文は英語で、日本語の字幕つけをお願いしています。</li>
       <li class="section_note_item">提出には、日本語で200字以内の概要、または300字以内の英語の概要が必要です。<br/>(採択後に、あらためて長めの概要の提出をお願いする場合があります。)</li>
