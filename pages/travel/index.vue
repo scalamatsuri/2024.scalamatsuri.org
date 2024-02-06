4a9bf2a94474b5113d7608d36f348facb54d18ba
omiend <omiend@gmail.com>
2024-02-06 10:42:51 +0900

    Revert "Revert "[Production] Release 2024-02-06 10:25:20""

---
+++ pages/travel/index.vue
@@ -0,0 +1,333 @@
+<i18n lang="yaml">
+en:
+  title: Travel
+  section_01_title: Book your flight
+  section_01_description: |
+    If you are coming from abroad, you will mostly likely fly into Tokyo.
+    There are two airports for Tokyo, <a href="https://www.tokyo-airport-bldg.co.jp/en/" target="_blank">Haneda Airport (code: HND)</a>, mostly serving domestic flights, and <a href="http://www.narita-airport.jp/en/index.html" target="_blank">Narita International Airport (code: NRT)</a>.
+    Haneda is 30 minutes from downtown Tokyo, and Narita is 70 minutes, so if there's a choice, fly to Haneda.<br/><br/>
+    If you're receiving travel support from us, please book your flight as soon as possible to get better rates.
+  section_02_title: Book your hotel
+  section_02_description: |
+    <br/>
+    Please book the hotel on your own. Here are some of the hotels near the conference venue.<br/><br/>
+    List of some hotels:<br/>
+  section_02_list:
+    - Tokyo Bay Ariake Washington Hotel(Recommended)　<a href="https://tokyobay.washington-hotels.jp/" target="_blank">English</a>
+    - Tokyu Stay Shimbashi　<a href="https://www.tokyustay.co.jp/e/hotel/SHI/" target="_blank">English</a>
+    - Mitsui Garden Hotel Shiodome Italia-gai　<a href="https://www.gardenhotels.co.jp/shiodome-italiagai/eng/" target="_blank">English</a>
+    - Sotetsu Grand Fresa Tokyo-Bay Ariake　<a href="https://sotetsu-hotels.com/en/grand-fresa/ariake/" target="_blank">English</a>
+    - Grand Nikko Tokyo Daiba　<a href="https://www.tokyo.grandnikko.com/eng/" target="_blank">English</a>
+  section_03_title: Getting to your hotel
+  section_03_description: |
+    If you are staying at Sotetsu Grand Fresa Tokyo-Bay Ariake, there's a bus every few hours operated by Airport Limousine that takes you from the terminal to the hotel.
+    <br/><br/>
+    <a href="https://webservice.limousinebus.co.jp/web/en/Top.aspx" target="_blank">webservice.limousinebus.co.jp</a>
+    <br/><br/>
+    If you are coming to Japan early or staying at another hotel, check the hotel's website for access information from the airports. A common way to get to Tokyo station is by using Narita Express (N'EX).
+  section_04_title: Tokyo International Exchange Center
+  section_04_description: |
+    ScalaMatsuri will take place at Tokyo International Exchange Center (東京国際交流館): a conference center in Tokyo that is equipped with facilities to host international conferences located in Odaiba area. This area is also called Rinkai Fukutoshin, or Tokyo Waterfront.
+    <br/><br/>
+    Address:
+    Tokyo International Exchange Center
+    Tokyo Academic Park, 2-2-1 Aomi, Koto-ku, Tokyo 135-8630, Japan
+    東京都江東区青海2-2-1　国際研究交流大学村内
+    <a href="https://www.jasso.go.jp/en/ryugaku/kyoten/tiec/access.html" target="_blank">Access</a>
+  section_05_title: Getting to the venue
+  section_05_description: |
+    Tokyo International Exchange Center is 2km away from Sotetsu Grand Fresa Tokyo-Bay Ariake, which is walkable. Probably the easiest way to get to the venue in the morning would be taking taxi especially if you can share the ride with others. But the taxi in Tokyo can be quite expensive.
+    <br/>
+    To take the public transportation, use Google Map to figure out what lines to take. From Sotetsu Grand Fresa Tokyo-Bay Ariake for example, take <a href="http://www.yurikamome.co.jp/info/" target="_blank">Yurikamome</a> line at U11 Tokyo Big Sight (東京ビッグサイト) towards Shimbashi (新橋), and get off at U08 Tokyo International Cruise Terminal (東京国際クルーズターミナル) station.
+  section_06_title: Odaiba / Tokyo Waterfront
+  section_06_description: |
+    <a href="https://www.gotokyo.org/en/index.html" target="_blank">Odaiba (お台場)</a> is a popular shopping and entertainment district on an artificial island in Tokyo Bay. There's also a life-size Gundam at a shopping mall called <a href="https://mitsui-shopping-park.com.e.act.hp.transer.com/divercity-tokyo/">DiverCity Tokyo Plaza</a>.
+  section_07_title: General tips
+  section_07_description: |
+    <p>
+      <a href="http://www.cnn.com/2014/04/06/travel/japan-travel-tips/" target="_blank">11 extremely practical Japan travel tips</a> is a good summary on general tips on traveling to Japan.
+      <a href="http://www.japan-guide.com/" target="_blank">japan-guide.com</a> has more detailed guide on various topics like <a href="http://www.japan-guide.com/e/e2017.html" target="_blank">Transportation in Tokyo</a>.
+      New York Times wrote an article <a href="http://www.nytimes.com/2015/11/01/travel/tokyo-solo-travel.html" target="_blank">Solo in Tokyo</a>, which you mind find interesting.
+    </p>
+
+    <h3 style="font-size:2rem;margin-top: 40px;margin-bottom: 20px;">
+      Book a data plan or a wireless router
+    </h3>
+    <p>
+      <a href="https://www.google.com/maps" target="_blank">Google Maps</a> is essential for both walking around and figuring out the transits.
+      Here's how you can get your smart phone to get connected in Japan.
+      <br/><br/>
+      <ul>
+        <li>T-mobile offers <a href="https://www.t-mobile.com/news/simple-global-data-passes"target="_blank" >$5 international day pass</a> (512 MB of high-speed data), 5GB (up to 10 days) for $35, and 15GB (up to 30 days) for $50.</li>
+        <li>Verizon offers international day pass for <a href="https://www.verizonwireless.com/plans/travelpass-countries/" target="_blank">$10/day</a>.</li>
+        <li>AT&T offers international day pass for <a href="https://www.att.com/offers/international-plans/day-pass.html" target="_blank">$10/day</a>.</li>
+      </ul>
+      <br/>
+      You can rent a wireless router, or "pocket wifi" device at the airport. This lets you use your laptops and smartphones.
+      Check the coverage of the plan if you're venturing out of the Tokyo metro area. Read the data plan carefully to avoid exceeding daily limit as the service provider can penalize the device by putting it on a slower connection.
+      <br/><br/>
+      <ul>
+        <li><a href="https://pupuruwifi.com" target="_blank">PuPuRu</a></li>
+        <li><a href="http://www.globaladvancedcomm.com/pocketwifi.html" target="_blank">Global Advanced Communications</a></li>
+      </ul>
+    </p>
+
+    <h3 style="font-size:2rem;margin-top: 40px;margin-bottom: 20px;">
+      Carry cash
+    </h3>
+    <p>
+      Japan is mostly cash-based country.
+      With a few exceptions like hotels, nice restaurants, train stations, and some taxi, do not expect your credit card to be accepted.
+      Even at a department store, foreign-issued credit cards may or may not work. Carry at least 10,000 JPY (around $100) at all times.
+      Use your ATM card to withdraw Japanese Yen. First, <i>contact your bank in advance</i> to activate foreign withdrawal (otherwise they can suspend the card thinking it's hacked).
+      There are <a href="https://www.citibank.co.jp/en/banking/branch_atm/" target="_blank">Citibank Japan</a>'s ATM machines at both Haneda and Narita airport.
+      Once you are in Tokyo, look for a <a href="http://www.sej.co.jp/in/en.html" target="_blank">7 Eleven</a> (Japanese 7 Eleven is so badass they operate their own bank) or a <a href="http://www.jp-bank.japanpost.jp/en/ias/en_ias_index.html" target="_blank">Japan Post</a> (Japanese postal office use to operate one of the largest banks in the world).
+    </p>
+
+    <h3 style="font-size:2rem;margin-top: 40px;margin-bottom: 20px;">
+      Buy Suica card
+    </h3>
+    <p>
+      <a href="https://www.jreast.co.jp/e/pass/suica.html" target="_blank">Suica</a> is a prepaid IC card by JR East. Suica (and other compatible cards like Pasmo and Kitaca) can be used for almost all trains and subways in major cities.
+      They can also be used for local bus lines, but we don't recommend you take buses since they are more involved, with the exception of the Airport Limousine bus (Unfortunately Suica card are accepted only on the inbound routes).
+      In addition, the IC cards can be used at convenient stores, vending machines, and coin lockers especially around train stations.
+    </p>
+
+    <h3 style="font-size:2rem;margin-top: 40px;margin-bottom: 20px;">
+      Power plugs
+    </h3>
+    <p>
+      See <a href="http://www.japan-guide.com/e/e2225.html" target="_blank">Electricity</a> page.
+      Japanese power outlets are identical to ungrounded (2-pin) North American outlets, but the voltage 100V, which is different from North America (120V), Central Europe (230V).
+      Many laptop adapters including the ones for MacBook Pro work in terms of the voltage.
+      Even if you are coming from North America, you might still need <a href="https://www.amazon.com/s?k=3-Prong-2-Prong-Electrical-Adapter&crid=3IDLHE5BBW2SK&sprefix=3-prong-2-prong-electrical-adapter+japan%2Caps%2C223&ref=nb_sb_noss" target="_blank">an adapter</a> to remove the earth pin from the extension cord.
+    </p>
+
+    <h3 style="font-size:2rem;margin-top: 40px;margin-bottom: 20px;">
+      Tipping
+    </h3>
+    <p>
+      In Japan, the service charges are included in the price. In other words, tipping is neither required nor accepted at hotels, restaurants, taxis etc.
+      The only exception that you might come across is if you stay at a better ryokan style inn, where you are attended to by one person for your entire visit.
+      The protocol is to put around 1000 JPY per guest in a small envelope (sold at a convenience store) and present to your attendant at the end of the in-room check-in process. This too is completely optional.
+    </p>
+    <p>
+      At a restaurant, instead of tipping, a common etiquette is to thank the chef, the waitor, or the host as you walk out of the restaurant by bowing slightly and saying <a href="https://www.youtube.com/watch?v=I0Rp1W8-ZSM&t=1m16s" target="_blank"><b>ごちそうさまでした</b></a> (gochisousama-deshita), "thank you for the lovely meal."
+      You can also say the same if someone buys you a meal.
+    </p>
+
+    <h3 style="font-size:2rem;margin-top: 40px;margin-bottom: 20px;">
+      Casual dining
+    </h3>
+    <p>
+      Beyond the usual sushi and tempura, there is an array of things you could try for more casual dining on the cheap.
+      See <a href="http://www.japan-guide.com/e/e2036.html" target="_blank">Restaurants</a> page on japan-guide.com for details.
+    </p>
+
+    <h3 style="font-size:2rem;margin-top: 40px;margin-bottom: 20px;">
+      Coffee
+    </h3>
+    <p>
+      The coffee quality can vary, but I generally find them weaker and sour in Japan.
+      Thankfully there are <a href="https://maps.app.goo.gl/NMqL3ckUKQCTwJ156" target="_blank">Starbucks chains</a> in Tokyo (Blue Bottle Coffee too but not too many).
+      Just type in <a href="https://www.google.com/maps/search/Starbucks+near+odaiba" target="_blank">"Starbucks near odaiba"</a> into Google Map.
+    </p>
+
+    <h3 style="font-size:2rem;margin-top: 40px;margin-bottom: 20px;">
+      Bakery
+    </h3>
+    <p>
+      Japanese style bakery sells interesting mix of sweet and savory pastries and bread that you won't find elsewhere.
+      Try curry bread (カレーパン), mellon bread (メロンパン), bacon pain d'epi (ベーコンエピ), or anything you think looks interesting.
+    </p>
+
+    <h3 style="font-size:2rem;margin-top: 40px;margin-bottom: 20px;">
+      Convenience store
+    </h3>
+    <p>
+      <a href="http://www.japan-guide.com/e/e2071.html" target="_blank">Convenience stores</a>, many open 24 hours a day, sell a large range of meals, such as onigiri (rice balls), sandwiches, obento (lunch boxes), and hot foods like fried chicken. For (lacto-) vegetarians, the fact that they have bread, yogurt, and salad helps as the last resort if you can't find food.
+    </p>
+
+    <h3 style="font-size:2rem;margin-top: 40px;margin-bottom: 20px;">
+      Soba
+    </h3>
+    <p>
+      Eating a lunch at a local soba shop is a truely Japanese experience for common folks, usually more reasonable than other fancier cuisine. There's usually a daily special that combines soba and one more dish.
+    </p>
+
+    <h3 style="font-size:2rem;margin-top: 40px;margin-bottom: 20px;">
+      Izakaya
+    </h3>
+    <p>
+      <a href="http://www.japan-guide.com/e/e2036_izakaya.html" target="_blank">Izakaya</a> (居酒屋) are casual drinking establishments, similar to tapas bars, where customers order a variety of small dishes of food that can be shared at the table.
+    </p>
+
+    <h3 style="font-size:2rem;margin-top: 40px;margin-bottom: 20px;">
+      Japanese curry
+    </h3>
+    <p>
+      Curry rice is one of the most popular dish cooked in Japanese households. Compared to the Indian or Thai variety they are usually on more milder side.
+    </p>
+
+    <h3 style="font-size:2rem;margin-top: 40px;margin-bottom: 20px;">
+      Ramen
+    </h3>
+    <p>
+      If you're looking for a quick meal or to top off the night after drinking, <a href="http://www.japan-guide.com/e/e2042.html" target="_blank">ramen</a> could be what you want.
+      <br/><br/>
+      For everyone who wants to try a more powerful ramen, I highly recommend experiencing the exquisite ramen at Ramen Jiro(ラーメン二郎). Ramen Jiro is known for its unique and rich pork bone soup, thick and chewy curly noodles, and generously topped ingredients that create a flavorful and hearty experience.
+      Each bowl of ramen comes loaded with a substantial amount of bean sprouts, cabbage, and pork belly, making for an enticing combination. Moreover, in the welcoming atmosphere of the restaurant, you can enjoy the distinctive ramen culture.
+      As part of experiencing Japan's culinary culture, be sure to pay a visit. The ramen at Ramen Jiro is a unique and satisfying example of food that you won't find elsewhere.
+      <br/>
+      <a href="https://en.wikipedia.org/wiki/Ramen_Jiro" target="_blank">Ramen Jiro(Wikipedia)</a>
+    </p>
+
+    <h3 style="font-size:2rem;margin-top: 40px;margin-bottom: 20px;">
+      Tonkatsu
+    </h3>
+    <p>
+      <a href="http://www.japan-guide.com/e/e2339.html" target="_blank">Tonkatsu</a> (とんかつ, "pork cutlet") are thick slices of pork that are breaded and deep fried in a manner similar to German schnitzel. It's a popular dinner option for dining out.
+    </p>
+
+    <h3 style="font-size:2rem;margin-top: 40px;margin-bottom: 20px;">
+      Questions?
+    </h3>
+    <p>
+      If you have any questions ask us using the <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSftjA6961ZzzJjqq1CrWtAN9wwrqXRUGFqZI5G8x2BjmZHeWw/viewform" target="_blank">inquiry form</a>.
+    </p>
+ja:
+  title: トラベル情報
+  section_01_title: ''
+  section_01_description: ''
+  section_02_title: ホテルの予約
+  section_02_description: |
+    <br/>
+    ホテルは各人で予約をお願いします。会場から近いホテルをピックアップしていますので参考にして下さい。<br/><br/>
+    ホテルのリスト:<br/>
+  section_02_list:
+    - 東京ベイ有明ワシントンホテル(推奨)　<a href="https://tokyobay.washington-hotels.jp/" target="_blank">English</a>
+    - 東急ステイ新橋　<a href="https://www.tokyustay.co.jp/e/hotel/SHI/" target="_blank">English</a>
+    - 三井ガーデンホテル汐留イタリア街　<a href="https://www.gardenhotels.co.jp/shiodome-italiagai/eng/" target="_blank">English</a>
+    - 相鉄グランドフレッサ 東京ベイ有明　<a href="https://sotetsu-hotels.com/en/grand-fresa/ariake/" target="_blank">English</a>
+    - グランドニッコー東京台場　<a href="https://www.tokyo.grandnikko.com/eng/" target="_blank">English</a>
+  section_03_title: ''
+  section_03_description: ''
+  section_04_title: 東京国際交流館
+  section_04_description: |
+    ScalaMatsuri は、東京国際交流館にて開催します。国際カンファレンスを開催できる設備をそなえた会場で、お台場エリアにあります。このエリアは臨海副都心などとも呼ばれています。<br/>
+    <br/>
+    東京国際交流館 プラザ平成<br>
+    東京都江東区青海2-2-1　国際研究交流大学村内<br>
+    <a href="https://www.jasso.go.jp/en/ryugaku/kyoten/tiec/access.html" target="_blank">アクセス</a>
+  section_05_title: 会場へのアクセス
+  section_05_description: |
+    東京国際交流館はホテルサンルート有明から 2km の距離で歩ける距離です。当日朝に会場へ行く一番簡単な方法は、特に複数の参加者で相乗りできればタクシーだと思います。<br/>
+    公共の交通機関を利用するには Google Map が便利です。例えば、相鉄グランドフレッサ 東京ベイ有明からは<b>U11 東京ビッグサイト</b>で<a href="http://www.yurikamome.co.jp/info/" target="_blank">ゆりかもめ</a>の新橋行きに乗って、<b>U08 東京国際クルーズターミナル</b>で降りてください。<br/>
+  section_06_title: お台場 / 臨海副都心
+  section_06_description: |
+    <a href="https://www.gotokyo.org/en/index.html" target="_blank">お台場</a>はショッピングや観光で人気な東京湾にある人工島です。<a href="https://mitsui-shopping-park.com.e.act.hp.transer.com/divercity-tokyo/">DiverCity Tokyo Plaza</a>というショッピングモールには実物大のガンダムがあることで有名です。
+  section_07_title: ''
+  section_07_description: ''
+</i18n>
+
+<template>
+  <MainVisual :title="t('title')" />
+  <div class="section">
+    <div v-if="t('section_01_title')">
+      <h2 class="section_title">
+        <span class="section_title_inner">{{ t('section_01_title') }}</span>
+      </h2>
+      <p class="section_text" v-html="t('section_01_description')" />
+      <br />
+      <br />
+      <br />
+    </div>
+
+    <h2 class="section_title">
+      <span class="section_title_inner">{{ t('section_02_title') }}</span>
+    </h2>
+    <p class="section_text" v-html="t('section_02_description')" />
+    <ul>
+      <li v-for="text in tm('section_02_list')" :key="text">
+        <div v-html="rt(text)"></div>
+      </li>
+    </ul>
+    <br />
+    <br />
+    <br />
+
+    <div v-if="t('section_03_title')">
+      <h2 class="section_title">
+        <span class="section_title_inner">{{ t('section_03_title') }}</span>
+      </h2>
+      <p class="section_text" v-html="t('section_03_description')" />
+      <br />
+      <br />
+      <br />
+    </div>
+
+    <h2 class="section_title">
+      <span class="section_title_inner">{{ t('section_04_title') }}</span>
+    </h2>
+    <p class="section_text" v-html="t('section_04_description')" />
+    <div class="venue_map">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1621.6413002621073!2d139.7761757!3d35.6207591!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601889fec03298c3%3A0x580ab4984e28a750!2z5p2x5Lqs5Zu96Zqb5Lqk5rWB6aSoIOODl-ODqeOCtuW5s-aIkA!5e0!3m2!1sja!2sjp!4v1703073639041!5m2!1sja!2sjp"
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    <br />
+    <br />
+    <br />
+
+    <h2 class="section_title">
+      <span class="section_title_inner">{{ t('section_05_title') }}</span>
+    </h2>
+    <p class="section_text" v-html="t('section_05_description')" />
+    <br />
+    <div class="venue_map">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d12972.271251968206!2d139.7707530430457!3d35.62605471168081!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e3!4m5!1s0x601889e8955bf575%3A0xc2021decfe9182eb!2z44Ot44O844K944OzIOebuOmJhOOCsOODqeODs-ODiSDjg5Xjg6zjg4PjgrXmnInmmI7lupc!3m2!1d35.6321671!2d139.7895725!4m3!3m2!1d35.6209524!2d139.7759719!5e0!3m2!1sja!2sjp!4v1705926930389!5m2!1sja!2sjp"
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    <br />
+    <br />
+    <br />
+
+    <h2 class="section_title">
+      <span class="section_title_inner">{{ t('section_06_title') }}</span>
+    </h2>
+    <p class="section_text" v-html="t('section_06_description')" />
+    <br />
+    <br />
+    <br />
+
+    <div v-if="t('section_07_title')">
+      <h2 class="section_title">
+        <span class="section_title_inner">{{ t('section_07_title') }}</span>
+      </h2>
+      <p class="section_text" v-html="t('section_07_description')" />
+      <br />
+      <br />
+      <br />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { t, tm, rt } = useI18n()
+pageMetaCheck()
+</script>
+
+<style lang="scss" scoped>
+.venue_map {
+  width: 100%;
+  height: 600px;
+  border: 0;
+  iframe {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
