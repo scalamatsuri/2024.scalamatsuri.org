c995a913a8d6d54340b9a539c255a29abe8ea38a
omiend <omiend@gmail.com>
2024-03-02 15:12:05 +0900

    update icon

--- components/staff/ListItem.vue
+++ components/staff/ListItem.vue
@@ -1,6 +1,7 @@
 <template>
   <li class="staff_item">
-    <div class="staff_img" :style="{ 'background-image': 'url(' + staff.backgroundImage + ')' }" />
+    <div v-if="staff.backgroundImage" class="staff_img" :style="{ 'background-image': 'url(' + staff.backgroundImage + ')' }" />
+    <div v-if="!staff.backgroundImage"><img class="staff_img" src="/img/common/logo.svg" alt="ScalaMatsuri Staff" /></div>
     <p class="staff_ruby">
       {{ staff.ruby }}
     </p>
@@ -32,7 +33,6 @@ const { staff } = defineProps({ staff: {} as PropType<Staff> })
 </script>
 
 <style lang="scss" scoped>
-
 .staff_item {
   width: 240px;
   text-align: center;
@@ -45,22 +45,22 @@ const { staff } = defineProps({ staff: {} as PropType<Staff> })
   background-size: cover;
 
   border-radius: 60px;
-  box-shadow:0px 0px 1px 1px rgba(0,0,0,0.1) inset;
+  box-shadow: 0px 0px 1px 1px rgba(0, 0, 0, 0.1) inset;
   margin: 0 auto 17px;
 }
 .staff_ruby {
-font-weight: bold;
-font-size: 14px;
-color: #999;
+  font-weight: bold;
+  font-size: 14px;
+  color: #999;
 }
 .staff_name {
-font-weight: bold;
-line-height: 29px;
-font-size: 18px;
-letter-spacing: 0.1em;
+  font-weight: bold;
+  line-height: 29px;
+  font-size: 18px;
+  letter-spacing: 0.1em;
 
-color: #000000;
-margin-bottom: 8px;
+  color: #000000;
+  margin-bottom: 8px;
 }
 
 .staff_sns {
@@ -74,10 +74,10 @@ margin-bottom: 8px;
   }
 }
 .staff_profile {
-line-height: 20px;
-font-size: 12px;
-text-align: center;
-color: #000000;
+  line-height: 20px;
+  font-size: 12px;
+  text-align: center;
+  color: #000000;
 }
 
 @media screen and (max-width: $viewport - 1) {
@@ -86,8 +86,6 @@ color: #000000;
     max-width: 50%;
     padding: 30px 10px 0;
     margin-bottom: 0;
-
   }
 }
-
 </style>
