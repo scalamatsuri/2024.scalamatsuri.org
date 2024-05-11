ebab57e8f150f513628b8751c1db5bbe0b740243
omiend <omiend@gmail.com>
2024-05-11 15:46:54 +0900

    オープン・マイク・カンファレンスページ作成

--- pages/open-mic-conference/index.vue
+++ pages/open-mic-conference/index.vue
@@ -1,16 +1,242 @@
 <i18n lang="yaml">
+## language=yaml
 en:
-  title: 'Open Mic Conference'
+  title: Open mic conference
+  timetable_title: |
+    Time table
+  timetable_description: |
+    Open mic conference - ScalaMatsuri 2023 Time table is <a href="https://github.com/orgs/scalamatsuri/projects/14" target="_blank" >here</a>.
+  what_is_an_open_mic_conference_title: What is an Open mic conference?
+  what_is_an_open_mic_conference_text_01: |
+    Have you ever wondered why all the sessions in Open mic conference Day are TBD? Might there be a problem? No. It's definitely intended!
+  what_is_an_open_mic_conference_text_02: |
+    An Open mic conference is a participant-driven meeting.
+    The term "unconference" has been applied, or self-applied, to a wide range of gatherings that try to avoid hierarchical aspects of a conventional conference, such as sponsored presentations and top-down organization.
+    <a href="https://en.wikipedia.org/wiki/Unconference" target="_blank" rel="noopener">Wikipedia</a>
+  what_is_an_open_mic_conference_text_03: |
+    If you felt that the timetable for the fly-in conference was filled with <b>TBD</b> and that it was a work in progress, you are correct.<br />
+    Let's see how ScalaMatsuri Open mic conference is organized.
+  flow_title: ScalaMatsuri Open mic conference flow
+  flow_text_01: |
+    Please register your session ideas at <a href="https://docs.google.com/forms/d/e/1FAIpQLSfAMtRJfErO8T6xXFGTdZLGXNQO0vKDkPg3KmO2LZpm0JWnTA/viewform" target="_blank" rel="noopener">Welcome to ScalaMatsuri 2024 Open Mic Conference (Google Forms)</a>!
+    <br />
+    We will review submissions according to the "Day-of Schedule" described below.
+    <br /><br />
+    Upon acceptance, we will confirm the following:
+    <br />
+    - Confirmation of availability for presentation<br/>
+    - Adjustment of session duration<br/>
+    - Slide review for <a href="https://scalamatsuri.org/en/code-of-conduct" target="_blank">Code of Conduct compliance</a><br/>
+    <br />
+    When the time comes, the presenter should go up on stage and start the session.<br /><br />
+    Additionally 🎉<br/>
+    As a token of appreciation for those who present at this year's Open Mic Conference, we will be giving away special commemorative original acrylic stands for ScalaMatsuri 2024 Open Mic Conference! <br />
+    We highly encourage you to participate! <br />
+    <br/>
+  flow_of_the_day_title: Flow of the day
+  flow_of_the_day_text_01: |
+    Please refer <a href="https://www.youtube.com/watch?v=XA3OqSml7V4" target="_blank" rel="noopener">here</a> for the morning meeting
+  flow_of_the_day_text_02: |
+    After the opening of the first day, all participants who came in the morning plan the timetable based on the material gathered the day before, and decide which time slot will be used.<br />
+    Day 2 will be similar, with timetable planning by all participants at the morning meeting.<br />
+    Once the timetable has been set, the flying conference sessions will begin!<br /><br />
+    You are free to join in or leave during the session, as there are a lot of interesting sessions and sessions you want to listen to often overlap.<br /><br />
+    The fly-in conference will feature a mixture of Japanese and English sessions.<br />
+    There is no simultaneous oral interpretation, but enjoy communication between Japanese and English speakers.<br /><br />
+    You can also jump in and add conference sessions outside of the morning and snack sessions. You may start a session in a guerrilla style.<br />
+    If you would like to be included in the timetable, please contact the management.<br />
+    The schedule for the day will be reflected on this page as soon as possible, so if you find a session that interests you, please feel free to join us even in the middle of the day.
+  themes_title: Call for sessions
+  themes_text_01: |
+    Scala has been used in a wide variety of fields and industries.<br />
+    What you want to learn might be what other participants have already known or experienced, and vice versa.<br />
+    Here are all Open mic conference idea samples.
+    <br/><br/>
+    - Since I know about FOO "a bit", may I answer your questions?<br/>
+    - I want to learn about BAR. Is there anyone who can talk about it?<br/>
+    - Let's play around BEEF together.<br/>
+    - Why not discuss BLAH-BLAR?<br/>
+    <br/>
+    You can ask what you want to hear, or propose what you want to talk bout.<br />
+    There are various session formats. Talk styles, workshops, hands-on, or panel discussions. You can propose your favorite format.<br /><br />
+    Also, this Open mic conference can be a wide variety of themes, such as themes that were not accepted,  your hobby projects, even or your lifestyle / career as a Scala engineer.<br />
+    Sessions like  anonymous questionnaires about salaries, and CTO masquerade panels were also hold in the past.<br />
+    Any trivial idea is fine, so let us see your ideas!
 ja:
-  title: '飛び入りカンファレンス'
+  title: オープン・マイク・カンファレンス
+  what_is_an_open_mic_conference_title: オープン・マイク・カンファレンスとは？
+  what_is_an_open_mic_conference_text_01: |
+    <a href="http://blog.scalamatsuri.org/entry/2015/09/29/023812" target="_blank" rel="noopener">番外編 Scala World 2015レポート その2 アンカンファレンス編</a>にも 紹介されているとおり、<b>参加者が全員でつくりあげる</b>カンファレンスです。
+  what_is_an_open_mic_conference_text_02: |
+    講演内容や発表者が当日まで決まっておらず、来場者が自分の話したい内容を発表する、参加者が全員でつくりあげるカンファレンスです。
+    発表したい人が、カンファレンス中に発表内容を提案し、参加者の投票によってプログラムが決まります。
+    <a href="http://d.hatena.ne.jp/keyword/%a5%a2%a5%f3%a5%ab%a5%f3%a5%d5%a5%a1%a5%ec%a5%f3%a5%b9" target="_blank" rel="noopener">Hatena Keyword</a>より
+  what_is_an_open_mic_conference_text_03: |
+    オープン・マイク・カンファレンスのタイムテーブルが<b>TBD</b>で埋まっていて、作りかけなんじゃないかと感じたあなた、正解です。
+    では、ScalaMatsuriのオープン・マイク・カンファレンスがどのように作られていくを説明します。
+  flow_title: オープン・マイク・カンファレンス登壇方法
+  flow_text_01: |
+    思い思いにセッションのアイディアを <a href="https://docs.google.com/forms/d/e/1FAIpQLSfAMtRJfErO8T6xXFGTdZLGXNQO0vKDkPg3KmO2LZpm0JWnTA/viewform" target="_blank" rel="noopener">Welcome to ScalaMatsuri 2024 Open Mic Conference(Google Forms)</a> から登録してください！
+    <br />
+    後述の「当日の流れ」に記載の方法で採択を行います。
+    <br /><br />
+    採択時に下記について確認をさせていただきます。
+    <br />
+    - 改めて登壇できるかどうかの確認<br/>
+    - セッション時間の調整<br/>
+    - スライドの<a href="https://scalamatsuri.org/en/code-of-conduct" target="_blank">CoCチェック</a><br/>
+    時間になったら登壇者はステージに上がり、セッションを開始してください。<br /><br />
+    さらに 🎉<br/>
+    今年のオープン・マイク・カンファレンスに登壇してくれた方には、ScalaMatsuri 2024 オープン・マイク・カンファレンス開催記念特製オリジナルアクリルスタンドをプレゼントします！<br />
+    是非ふるってご参加ください！<br />
+    <br/>
+  flow_of_the_day_title: 当日の流れ
+  flow_of_the_day_text_01: |
+    <b>過去の朝会</b>の様子は、<a href="https://www.youtube.com/watch?v=XA3OqSml7V4" target="_blank" rel="noopener">こちら</a>をご覧ください。
+  flow_of_the_day_text_02: |
+    1日目のオープニングの後、前日までに集まったネタを元に朝から来てくれた参加者のみんなでタイムテーブルのプランニングを行い、どの時間帯で開催するかを決めます。<br />
+    2日目も同様に、朝会にて参加者のみんなでタイムテーブルのプランニングを行います。<br /><br />
+    タイムテーブルが決まったらオープン・マイク・カンファレンスのセッションが始まります！<br />
+    面白いセッションが目白押しで、聞きたいセッションがかぶることも多いので、途中参加、途中退出は自由です。<br />
+    オープン・マイク・カンファレンスでは、日本語や英語のセッションが入り混じります。<br />
+    口頭での同時通訳はありませんが、日英話者間でのコミュニケーションをお楽しみください。<br /><br />
+    また、朝会・おやつ会以外でもオープン・マイク・カンファレンスのセッション追加は可能です。ゲリラ的にセッションを始めていただいても構いません。<br /><br />
+    タイムテーブルに掲載したい場合は、運営にお声がけください。<br /><br />
+    当日のスケジュールは随時このページに反映されますので、興味を惹かれるセッションがあったら是非途中からでもご参加ください。
+  themes_title: ネタ出し
+  themes_text_01: |
+    Scalaが使われている分野や業種は、幅広くなりました。
+    自分たちが困っていたり知っていることは、実は他の人が既に知っていたり、他の人からすると喉から手がでるほど欲しい情報だったりします。<br />
+    それらが全てオープン・マイク・カンファレンスのネタになります！
+    <br /><br />
+    - ○○について知ってるけどなんか知りたいことある？<br/>
+    - ○○について知りたい！<br/>
+    - ○○を使ってみんなで一緒にやろうぜ<br/>
+    - ○○についていろいろ議論したいから集まろうぜ<br/>
+    <br />
+    など、自分が発表出来るものでも良いし、ただ聞いてみたいものでもOKです。きっと誰かが教えてくれます。<br />
+    セッションの形式も、一人が話すトーク形式だけでなく、ワークショップやハンズオン、パネルディスカッションなど様々な形式で大丈夫です。<br /><br />
+    また、カンファレンスでは惜しくも選考されなかったネタや、誰得だよというマニアックなネタ、更にはScalaエンジニアとしての生き方などまで、多種多様なネタもウェルカムです。<br />
+    過去には、みんなに年収を聞くセッションや、CTO仮面座談会なども開催されています。<br />
+    どんな些細なネタでもいいので出して見て下さい。
 </i18n>
 
 <template>
   <MainVisual :title="t('title')" />
-  {{ $route.fullPath }}
+  <h3 class="sub_header_link">
+    <NuxtLink v-for="timetable in timetables" :key="'l_' + timetable.timetableId" :to="'#' + timetable.timetableId" class="sub_header_link_item">
+      {{ timetable.title[locale] }}
+    </NuxtLink>
+  </h3>
+  <div class="section">
+    <h2 class="section_title">
+      <span class="section_title_inner">{{ t('what_is_an_open_mic_conference_title') }}</span>
+    </h2>
+    <p class="section_text" v-html="t('what_is_an_open_mic_conference_text_01')" />
+    <blockquote class="section_text" v-html="t('what_is_an_open_mic_conference_text_02')" />
+    <p class="section_text" v-html="t('what_is_an_open_mic_conference_text_03')" />
+  </div>
+  <div class="section">
+    <h2 class="section_title">
+      <span class="section_title_inner">{{ t('flow_title') }}</span>
+    </h2>
+    <p class="section_text" v-html="t('flow_text_01')" />
+    <div class="open-mic-conference">
+      <img
+        src="/img/open-mic-conference/OpenMicConferencePresent.png"
+        alt="ScalaMatsuri 2024 オープン・マイク・カンファレンス開催記念特製オリジナルアクリルスタンド"
+        width="100%" />
+    </div>
+  </div>
+  <div class="section">
+    <h2 class="section_title">
+      <span class="section_title_inner">{{ t('flow_of_the_day_title') }}</span>
+    </h2>
+    <p class="section_text" v-html="t('flow_of_the_day_text_01')" />
+    <br /><br />
+    <figure class="section_figure">
+      <iframe
+        class="section_movie"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/XA3OqSml7V4"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen />
+    </figure>
+    <p class="section_text" v-html="t('flow_of_the_day_text_02')" />
+  </div>
+  <div class="section">
+    <h2 class="section_title">
+      <span class="section_title_inner">{{ t('themes_title') }}</span>
+    </h2>
+    <p class="section_text" v-html="t('themes_text_01')" />
+  </div>
+  <!-- TimeTable -->
+  <ClientOnly>
+    <div class="proposals">
+      <div v-for="timetable in timetables" :key="timetable.timetableId">
+        <ProgramDetail :sessions="sessions" :events="timetableEvents" :timetable="timetable" />
+      </div>
+    </div>
+  </ClientOnly>
 </template>
 
 <script setup lang="ts">
+import { type TimeTableEvent } from '~/models/model'
 const { t } = useI18n()
 pageMetaCheck()
+const sessions = getOMCSessions()
+const timetableEvents: Array<TimeTableEvent> = []
+const timetables = getOMCTimetables()
 </script>
+
+<style scoped lang="scss">
+.sub_header_link {
+  margin-top: 60px;
+  text-align: center;
+  padding: 40px;
+  background-color: #f1f1f1;
+}
+/* footer PC */
+@media screen and (min-width: $headerViewport) {
+  .sub_header_link_item {
+    display: inline-block;
+    margin: 0 20px;
+  }
+}
+/* footer SP */
+@media screen and (max-width: $headerViewport - 1) {
+  .sub_header_link_item {
+    display: block;
+    margin: 20px 0;
+  }
+}
+
+// .page {
+//   text-align: center;
+//   margin: 0 auto;
+//   &.is_disabled {
+//     display: none;
+//   }
+// }
+// .page_title {
+//   margin-top: 100px;
+//   padding: 10px 59px;
+//   font-weight: bold;
+//   line-height: 49px;
+//   font-size: 30px;
+//   text-align: center;
+//   letter-spacing: 0.05em;
+//   color: #fff;
+//   background: #333;
+//   display: inline-block;
+// }
+.proposals {
+  margin: 60px auto 0;
+  max-width: 1200px;
+}
+.open-mic-conference img {
+  width: 50rem;
+}
+</style>
