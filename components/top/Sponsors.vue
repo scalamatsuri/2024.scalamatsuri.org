11a379626e500c20e38c7186837acc2ea43b52de
omiend <omiend@gmail.com>
2024-05-26 15:36:34 +0900

    add gofuku bugyo

--- components/top/Sponsors.vue
+++ components/top/Sponsors.vue
@@ -17,6 +17,7 @@ en:
   bugyo_hatena: 'Kawara-ban Bugyo'
   bugyo_sentry: 'Metsuke Bugyo'
   bugyo_scalatimes: 'Kawara-ban Bugyo'
+  gofuku_bugyo: The ScalaMatsuri 2024 commemorative T-shirt was designed and produced by FOLIO Co., Ltd.
 ja:
   sponsor_title: 'スポンサー'
   sponsor_overview1: アジア最大級の国際Scalaカンファレンスである、ScalaMatsuriに協賛いただけるスポンサー様を募集しています。
@@ -35,6 +36,7 @@ ja:
   bugyo_hatena: '瓦版奉行'
   bugyo_sentry: '目付奉行'
   bugyo_scalatimes: '瓦版奉行'
+  gofuku_bugyo: ScalaMatsuri 2024 開催記念Tシャツは株式会社FOLIO様にデザイン・制作いただきました。
 </i18n>
 
 <template>
@@ -52,88 +54,73 @@ ja:
         <ul class="sponsors_list">
           <li v-for="sponsor in shuffledShoguns" :key="sponsor.logo" class="sponsors_item">
             <a :href="sponsor.url" target="_blank"><img :src="sponsor.logo" :alt="sponsor.en.name" /></a>
-            <div v-if="locale == `en`">
-              <p>{{ sponsor.en.name }}</p>
-            </div>
-            <div v-if="locale == `ja`">
-              <p>{{ sponsor.ja.name }}</p>
-            </div>
+            <div v-if="locale == `en`"><p>{{ sponsor.en.name }}</p></div>
+            <div v-if="locale == `ja`"><p>{{ sponsor.ja.name }}</p></div>
           </li>
         </ul>
         <h3 class="sponsors_title sponsors_subtitle-tairou">{{ t('tairou') }}</h3>
         <ul class="sponsors_list">
           <li v-for="sponsor in shuffledTairous" :key="sponsor.logo" class="sponsors_item">
             <a :href="sponsor.url" target="_blank"><img :src="sponsor.logo" :alt="sponsor.en.name" /></a>
-            <div v-if="locale == `en`">
-              <p>{{ sponsor.en.name }}</p>
-            </div>
-            <div v-if="locale == `ja`">
-              <p>{{ sponsor.ja.name }}</p>
-            </div>
+            <div v-if="locale == `en`"><p>{{ sponsor.en.name }}</p></div>
+            <div v-if="locale == `ja`"><p>{{ sponsor.ja.name }}</p></div>
           </li>
         </ul>
         <h3 class="sponsors_title sponsors_subtitle-daimyo">{{ t('daimyo') }}</h3>
         <ul class="sponsors_list">
           <li v-for="sponsor in shuffledDaimyos" :key="sponsor.logo" class="sponsors_item">
             <a :href="sponsor.url" target="_blank"><img :src="sponsor.logo" :alt="sponsor.en.name" /></a>
-            <div v-if="locale == `en`">
-              <p>{{ sponsor.en.name }}</p>
-            </div>
-            <div v-if="locale == `ja`">
-              <p>{{ sponsor.ja.name }}</p>
-            </div>
+            <div v-if="locale == `en`"><p>{{ sponsor.en.name }}</p></div>
+            <div v-if="locale == `ja`"><p>{{ sponsor.ja.name }}</p></div>
           </li>
         </ul>
         <h3 class="sponsors_title sponsors_subtitle-samurai">{{ t('samurai') }}</h3>
         <ul class="sponsors_list">
           <li v-for="sponsor in shuffledSamurais" :key="sponsor.logo" class="sponsors_item">
             <a :href="sponsor.url" target="_blank"><img :src="sponsor.logo" :alt="sponsor.en.name" /></a>
-            <div v-if="locale == `en`">
-              <p>{{ sponsor.en.name }}</p>
-            </div>
-            <div v-if="locale == `ja`">
-              <p>{{ sponsor.ja.name }}</p>
-            </div>
+            <div v-if="locale == `en`"><p>{{ sponsor.en.name }}</p></div>
+            <div v-if="locale == `ja`"><p>{{ sponsor.ja.name }}</p></div>
           </li>
         </ul>
         <h3 class="sponsors_title sponsors_subtitle-utage">{{ t('utage') }}</h3>
         <ul class="sponsors_list">
           <li v-for="sponsor in shuffledUtages" :key="sponsor.logo" class="sponsors_item">
             <a :href="sponsor.url" target="_blank"><img :src="sponsor.logo" :alt="sponsor.en.name" /></a>
-            <div v-if="locale == `en`">
-              <p>{{ sponsor.en.name }}</p>
-            </div>
-            <div v-if="locale == `ja`">
-              <p>{{ sponsor.ja.name }}</p>
-            </div>
+            <div v-if="locale == `en`"><p>{{ sponsor.en.name }}</p></div>
+            <div v-if="locale == `ja`"><p>{{ sponsor.ja.name }}</p></div>
           </li>
         </ul>
         <h3 class="sponsors_title sponsors_subtitle-ninja">{{ t('ninja') }}</h3>
         <ul class="sponsors_list">
           <li v-for="sponsor in shuffledNinjas" :key="sponsor.logo" class="sponsors_item-ninja">
             <a :href="sponsor.url" target="_blank"><img :src="sponsor.logo" :alt="sponsor.en.name" /></a>
-            <div v-if="locale == `en`">
-              <p>{{ sponsor.en.name }}</p>
-            </div>
-            <div v-if="locale == `ja`">
-              <p>{{ sponsor.ja.name }}</p>
-            </div>
+            <div v-if="locale == `en`"><p>{{ sponsor.en.name }}</p></div>
+            <div v-if="locale == `ja`"><p>{{ sponsor.ja.name }}</p></div>
           </li>
         </ul>
         <h3 class="sponsors_title sponsors_subtitle-bugyo">{{ t('bugyo') }}</h3>
         <ul class="sponsors_list">
           <li v-for="sponsor in shuffledBugyos" :key="sponsor.logo" class="sponsors_item-bugyo">
-            <a :href="sponsor.url" target="_blank">
-              <img :src="sponsor.logo" :alt="sponsor.en.name" />
-            </a>
-            <div v-if="locale == `en`">
-              <p>{{ sponsor.en.name }}</p>
-            </div>
-            <div v-if="locale == `ja`">
-              <p>{{ sponsor.ja.name }}</p>
-            </div>
+            <a :href="sponsor.url" target="_blank"><img :src="sponsor.logo" :alt="sponsor.en.name" /></a>
+            <div v-if="locale == `en`"><p>{{ sponsor.en.name }}</p></div>
+            <div v-if="locale == `ja`"><p>{{ sponsor.ja.name }}</p></div>
           </li>
         </ul>
+        <!-- ScalaMatsuri 2024 特別実装 -->
+        <div class="gofuku-bugyo">
+          <div class="gofuku-bugyo-title">
+            <div v-if="locale == `en`"><p>Gohuku Bugyo</p></div>
+            <div v-if="locale == `ja`"><p>呉服奉行</p></div>
+          </div>
+          <div class="gofuku-bugyo-logo">
+            <a href="https://corp.folio-sec.com/" target="_blank"><img src="/img/sponsors/folio.svg" alt="sponsor.en.name" /></a>
+          </div>
+          <div class="gofuku-bugyo-content">
+            <img class="gofuku-bugyo_img" src="/img/top/t-shirts/t-shirts-01.png" alt="sponsor.en.name" />
+            <img class="gofuku-bugyo_img" src="/img/top/t-shirts/t-shirts-02.png" alt="sponsor.en.name" />
+          </div>
+          <p>{{ t("gofuku_bugyo") }}</p>
+        </div>
       </div>
     </section>
   </ClientOnly>
@@ -159,17 +146,67 @@ const shuffledBugyos = computed(() => arrayShuffle(bugyos.value))
 </script>
 
 <style lang="scss" scoped>
+.gofuku-bugyo {
+  &-title {
+    font-size: 1.5em;
+    margin-top: 1em;
+    font-weight: bold;
+  }
+  &-logo {
+    width: 220px;
+    margin: 0 auto;
+    img {
+      margin-top: 1em;
+      width: 100%;
+      background: white;
+    }
+  }
+}
+@media screen and (min-width: $headerViewport) {
+  /* footer PC */
+  .gofuku-bugyo {
+    &-content {
+      width: 30%;
+      margin: 0 auto;
+      display: flex;
+      justify-content: center;
+      margin-top: 1em;
+      max-width: 600px;
+      img {
+        width: 80%;
+        padding: 10px;
+        margin: 0px 10px;
+        background: white;
+      }
+    }
+  }
+}
+@media screen and (max-width: $headerViewport - 1) {
+  /* footer SP */
+  .gofuku-bugyo {
+    &-content {
+      // width: 30%;
+      margin: 0 auto;
+      justify-content: center;
+      img {
+        width: 60%;
+        padding: 10px;
+        margin-top: 10px;
+        background: white;
+      }
+    }
+  }
+}
+
 .content_title {
   display: inline-block;
   margin-top: 40px;
   font-weight: bold;
-  font-size: 48px;
   color: #bd3844;
 }
 
 .sponsors_title {
   margin-top: 1em;
-  font-size: 48px;
   font-weight: bold;
 }
 .sponsors_content_text {
@@ -214,7 +251,7 @@ const shuffledBugyos = computed(() => arrayShuffle(bugyos.value))
 .sponsors {
   background-color: #f1f1f1;
   text-align: center;
-  padding: 58px 0 100px;
+  padding: 1rem 0;
 }
 .sponsors_list {
   display: flex;
